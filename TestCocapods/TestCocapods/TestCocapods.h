ec3859757990f743c5df08fb8a4688df39967ad4
ThanhND <thanhnd@ThanhND-GD-M.local>
2022-06-30 09:10:19 +0700

    init project

---
+++ TestCocapods/TestCocapods/TestCocapods.h
@@ -0,0 +1,18 @@
+//
+//  TestCocapods.h
+//  TestCocapods
+//
+//  Created by ThanhND on 6/30/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestCocapods.
+FOUNDATION_EXPORT double TestCocapodsVersionNumber;
+
+//! Project version string for TestCocapods.
+FOUNDATION_EXPORT const unsigned char TestCocapodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCocapods/PublicHeader.h>
+
+
